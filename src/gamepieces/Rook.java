1dcc015ff6e4af77845b583950dbe57538fd13f2
Areii1 <ari.jaaskelainen1@gmail.com>
2016-02-09 01:56:20 +0200

    Added legal moves for Rook class.

--- src/gamepieces/Rook.java
+++ src/gamepieces/Rook.java
@@ -7,5 +7,18 @@ public class Rook extends Gamepiece {
 	public Rook(int x, int y, Player player) {
 		super(x, y, player, 'b');
 	}
-
+	
+	public void move(int newX, int newY) {
+		if (isRetardMove(x, newX, y, newY)) return;
+		
+		super.move(newX, newY);
+	}
+	
+	private boolean isXAndYChanged(int x, int newX, int y, int newY) {
+		return x - newX != 0 && y - newY != 0;
+	}
+	
+	private boolean isRetardMove(int x, int newX, int y, int newY) {
+		return isXAndYChanged(x, newX, y, newY);
+	}
 }
--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -11,16 +11,34 @@ public class BoardTest {
 		
 		Player player = new Player();
 		
-		Pawn pawn1 = new Pawn(8, 7, player);
-		Pawn pawn2 = new Pawn(1, 6, player);
+		Pawn pawn1 = new Pawn(1, 7, player);
+		Pawn pawn2 = new Pawn(2, 7, player);
+		Pawn pawn3 = new Pawn(3, 7, player);
+		Pawn pawn4 = new Pawn(4, 7, player);
+		Pawn pawn5 = new Pawn(5, 7, player);
+		Pawn pawn6 = new Pawn(6, 7, player);
+		Pawn pawn7 = new Pawn(7, 7, player);
+		Pawn pawn8 = new Pawn(8, 7, player);
+		
+		Rook rook1 = new Rook(1, 8, player);
+		Rook rook2 = new Rook(8, 8, player);
 	
 		chessboard.addToBoard(pawn1);
 		chessboard.addToBoard(pawn2);
+		chessboard.addToBoard(pawn3);
+		chessboard.addToBoard(pawn4);
+		chessboard.addToBoard(pawn5);
+		chessboard.addToBoard(pawn6);
+		chessboard.addToBoard(pawn7);
+		chessboard.addToBoard(pawn8);
+		
+		chessboard.addToBoard(rook1);
+		chessboard.addToBoard(rook2);
 		
 		chessboard.render();
 		System.out.println();
-		pawn1.move(2, 7);
-		pawn2.move(3, 4);
+		
+		chessboard.moveGamepiece(rook2, 8, 6);
 		
 		chessboard.render();
 	}
