6d69682558909c239cb670ad73140384b747b5ad
Areii1 <ari.jaaskelainen1@gmail.com>
2016-02-08 21:41:43 +0200

    Created a method for Chessboard class that renders a chessboard.

--- src/game/Chessboard.java
+++ src/game/Chessboard.java
@@ -3,5 +3,13 @@ package game;
 public class Chessboard {
 	
 //protected Square[] tiles = new Square[];
-
+	
+	public void render() {
+		for (int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				System.out.print(" X ");
+			}
+			System.out.println("");
+		}
+	}
 }
---
+++ src/tests/BoardTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import game.*;
+
+public class BoardTest {
+	
+	public static void main(String[] args) {
+		
+		Chessboard chessboard = new Chessboard();
+		
+		chessboard.render();
+	}
+
+}
