b009e5deaa690713282e1208855ad43f61dd5c35
Joonas <makinen.jns@gmail.com>
2016-02-16 19:01:17 +0200

    Created moving conditions for queen.

--- src/gamepieces/Queen.java
+++ src/gamepieces/Queen.java
@@ -1,11 +1,26 @@
 package gamepieces;
 
+import static game.MoveValidation.isXAndYChanged;
+import static game.MoveValidation.isXAndYEvenChange;
+
 import game.*;
 
 public class Queen extends Gamepiece{
 	
 	public Queen(int x, int y, Player player) {
 		super(x, y, player, 'Q');
+	}
+	public void move(int newX, int newY) {
+		if (isRetardMove(x, y, newX, newY)) return;
+		
+		super.move(newX, newY);
+		
+	}
+	private boolean isRetardMove(int x, int y, int newX, int newY){
+		System.out.println(!isXAndYEvenChange(x, newX, y, newY));
+		System.out.println(isXAndYChanged(x, newX, y, newY));
+		return (!isXAndYEvenChange(x, newX, y, newY)&&
+				isXAndYChanged(x, newX, y, newY));
 	}
 
 }
